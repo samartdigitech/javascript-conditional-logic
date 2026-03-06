ed7f128e9a59a425f11dfd7c90be4aae905684dd
SAMART NUANPAKSEE <admin@samartdigitech.com>
2026-03-06 22:18:40 +0700

    resolve exercise 6 and 7

--- ex-6.js
+++ ex-6.js
@@ -3,7 +3,8 @@ let favoriteBooks = [];
 function addFavoriteBook(bookName) {
   // Start coding here !
   if (bookName.includes("Beauty")){
-    favoriteBooks.push(bookName)
+    favoriteBooks.push(bookName);
+    return;
   }
 }
 
--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,8 @@
 //Start coding here
-const greeting = "Hello ! 😁";
+const greeting = function(){
+    console.log("Hello ! 😁");
+};
 
-console.log(greeting);
+greeting();
 
 console.log("This line will be executed without any error.");
